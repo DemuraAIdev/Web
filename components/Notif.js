361fd3e4ac11d305319857d1c59b1a18e01588b2
DemuraAIdev <cyberaioff@gmail.com>
2022-09-09 20:01:09 +0700

    Notif Fix Bug

--- components/Notif.js
+++ components/Notif.js
@@ -3,7 +3,7 @@ export default function notif() {
   return (
     <div className="text-center">
       <div
-        className="flex items-center bg-indigo-800 p-2 leading-none text-indigo-100 lg:inline-flex lg:rounded-full"
+        className="inline-flex items-center rounded-full bg-indigo-800 p-2 leading-none text-indigo-100"
         role="alert"
       >
         <span className="mr-3 flex rounded-full bg-indigo-500 px-2 py-1 text-xs font-bold uppercase">
