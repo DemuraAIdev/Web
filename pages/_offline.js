088ab3ede4515bf5488f471d41f52b8c52345975
DemuraAIdev <cyberaioff@gmail.com>
2022-09-11 13:05:54 +0700

    offline page

---
+++ pages/_offline.js
@@ -0,0 +1,24 @@
+/* eslint-disable prettier/prettier */
+import Link from '@/components/Link'
+import { PageSEO } from '@/components/SEO'
+import siteMetadata from '@/data/siteMetadata'
+
+export default function FourZeroFour() {
+  return (
+    <>
+      <PageSEO title={`Page Not Found - ${siteMetadata.title}`} />
+      <div className="flex flex-col items-start justify-start md:mt-24 md:flex-row md:items-center md:justify-center md:space-x-6">
+        <div className="space-x-2 pt-6 pb-8 md:space-y-5">
+          <h1 className="text-6xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 md:border-r-2 md:px-6 md:text-8xl md:leading-14">
+            You are Offline
+          </h1>
+        </div>
+        <div className="max-w-md">
+          <p className="mb-4 text-xl font-bold leading-normal md:text-2xl">
+            Please Connect to the internet
+          </p>
+        </div>
+      </div>
+    </>
+  )
+}
