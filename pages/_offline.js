3a975c495f5d49eeec136e3f1790845b50692ea8
DemuraAIdev <cyberaioff@gmail.com>
2022-09-11 13:43:11 +0700

    uhhli

--- pages/_offline.js
+++ pages/_offline.js
@@ -9,7 +9,7 @@ export default function Offline() {
       <div className="flex flex-col items-start justify-start md:mt-24 md:flex-row md:items-center md:justify-center md:space-x-6">
         <div className="space-x-2 pt-6 pb-8 md:space-y-5">
           <h1 className="text-4xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 md:border-r-2 md:px-6 md:text-8xl md:leading-14">
-            You are Offline
+            Offline
           </h1>
         </div>
         <div className="max-w-md">
