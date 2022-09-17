c4973c13e58325d64c70cb5c833accb63545488b
DemuraAIdev <cyberaioff@gmail.com>
2022-09-17 10:10:02 +0700

    awddawadw

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -20,8 +20,9 @@ export function middleware(req, ev) {
   response.headers.set('Referrer-Policy', 'strict-origin-when-cross-origin')
   response.headers.set('Strict-Transport-Security', 'max-age=31536000; includeSubDomains; preload')
   response.headers.set('X-Content-Type-Options', 'nosniff')
-  response.headers.set('Access-Control-Allow-Methods', 'GET, DELETE, POST, OPTIONS')
-  response.headers.set('Access-Control-Allow-Origin', 'https://umami.vahryiskandar.my.id')
+  response.headers.set('Access-Control-Allow-Methods', 'GET, DELETE, POST, OPTIONS, HEAD')
+  response.headers.set('Access-Control-Allow-Origin', '*')
+  response.headers.set('Access-Control-Allow-Credentials', 'true')
   response.headers.set('X-DNS-Prefetch-Control', 'on')
   response.headers.set('Permissions-Policy', 'geolocation=(self), microphone=()')
 
