4af91ed80791161d12c3b09d4c6c63ca8e2a9861
DemuraAIdev <cyberaioff@gmail.com>
2022-09-17 12:45:17 +0700

    Many Icon

--- components/Footer.js
+++ components/Footer.js
@@ -35,7 +35,7 @@ export default function Footer() {
           ))}
         </div>
 
-        <div className="flex flex-col space-y-4">
+        <div className="flex flex-col space-y-4 ">
           {SocialLink.map((link) => (
             <Link
               key={link.title}
@@ -43,19 +43,6 @@ export default function Footer() {
               className=" text-gray-400 transition hover:text-primary-400"
             >
               {link.title}
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                className="ml-0.5 inline-block h-4 w-4 fill-current"
-              >
-                <g data-name="Layer 2">
-                  <g data-name="external-link">
-                    <rect width="24" height="24" opacity="0"></rect>
-                    <path d="M20 11a1 1 0 0 0-1 1v6a1 1 0 0 1-1 1H6a1 1 0 0 1-1-1V6a1 1 0 0 1 1-1h6a1 1 0 0 0 0-2H6a3 3 0 0 0-3 3v12a3 3 0 0 0 3 3h12a3 3 0 0 0 3-3v-6a1 1 0 0 0-1-1z"></path>
-                    <path d="M16 5h1.58l-6.29 6.28a1 1 0 0 0 0 1.42 1 1 0 0 0 1.42 0L19 6.42V8a1 1 0 0 0 1 1 1 1 0 0 0 1-1V4a1 1 0 0 0-1-1h-4a1 1 0 0 0 0 2z"></path>
-                  </g>
-                </g>
-              </svg>
             </Link>
           ))}
         </div>
