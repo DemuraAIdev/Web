e382e4ad7bafaaebad41e92bb3ad088ab8887302
DemuraAIdev <cyberaioff@gmail.com>
2022-11-02 11:10:18 +0700

    Tags Comeback

--- components/Tag.js
+++ components/Tag.js
@@ -1,12 +1,14 @@
-import Link from 'next/link'
-import kebabCase from '@/lib/utils/kebabCase'
+import Link from "next/link";
+import kebabCase from "@/lib/utils/kebabCase";
 
 const Tag = ({ text }) => {
   return (
-    <p className="mr-3 text-sm font-medium uppercase text-primary-500 hover:text-primary-600 dark:hover:text-primary-400">
-      {text.split(' ').join('-')}
-    </p>
-  )
-}
+    <Link href={`/tags/${kebabCase(text)}`} passHref>
+      <p className="mr-3 text-sm font-medium uppercase text-primary-500 hover:text-primary-600 dark:hover:text-primary-400">
+        {text.split(" ").join("-")}
+      </p>
+    </Link>
+  );
+};
 
-export default Tag
+export default Tag;
--- data/footerLinks.js
+++ data/footerLinks.js
@@ -1,21 +1,23 @@
-/* eslint-disable prettier/prettier */
-import siteMetadata from '@/data/siteMetadata'
+import siteMetadata from "@/data/siteMetadata";
 const Portfolio = [
-  { href: '/', title: 'Home' },
-  { href: '/blog', title: 'Blog' },
-  { href: '/projects', title: 'Projects' },
-  { href: '/about', title: 'About' },
-  { href: '/guestbook', title: 'Guestbook' },
-]
+  { href: "/", title: "Home" },
+  { href: "/blog", title: "Blog" },
+  { href: "/projects", title: "Projects" },
+  { href: "/about", title: "About" },
+  { href: "/guestbook", title: "Guestbook" },
+];
 export const Other = [
-  { href: '/newsletter', title: 'Newsletter' },
-  { href: 'https://bin.vahryiskandar.my.id/', title: 'Bin' },
-  { href: 'https://uptime.vahryiskandar.my.id/', title: 'Uptime' },
-  { href: 'https://umami.vahryiskandar.my.id/share/LNdNPeZv/Utma', title: 'Analytics' },
-]
+  { href: "/newsletter", title: "Newsletter" },
+  { href: "https://bin.vahryiskandar.my.id/", title: "Bin" },
+  { href: "https://uptime.vahryiskandar.my.id/", title: "Uptime" },
+  {
+    href: "https://umami.vahryiskandar.my.id/share/LNdNPeZv/Utma",
+    title: "Analytics",
+  },
+];
 export const SocialLink = [
-  { href: siteMetadata.github, title: 'GIthub' },
-  { href: siteMetadata.twitter, title: 'Twitter' },
-]
+  { href: siteMetadata.github, title: "GIthub" },
+  { href: siteMetadata.twitter, title: "Twitter" },
+];
 
-export default Portfolio
+export default Portfolio;
