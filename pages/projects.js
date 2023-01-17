f4366748c53bab13657518131b51046924f9f932
DemuraAIdev <cyberaioff@gmail.com>
2023-01-17 19:08:23 +0700

    lang for Projects

--- pages/projects.js
+++ pages/projects.js
@@ -2,12 +2,14 @@ import siteMetadata from "@/data/siteMetadata";
 import projectsData from "@/data/projectsData";
 import Card from "@/components/Card";
 import { PageSEO } from "@/components/SEO";
+import useTranslation from "next-translate/useTranslation";
 
 export async function getStaticProps({ locale, locales }) {
   return { props: { locale, availableLocales: locales } };
 }
 
 export default function Projects({ locale, availableLocales }) {
+  const { t } = useTranslation();
   return (
     <>
       <PageSEO
@@ -18,10 +20,10 @@ export default function Projects({ locale, availableLocales }) {
       <div className="">
         <div className="space-y-2 pt-6 pb-8 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-            Projects
+            {t("pro")}
           </h1>
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
-            MY workssssss!
+            {t("desc")}
           </p>
         </div>
         <div className="container py-12">
