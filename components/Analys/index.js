0e18e0c1eaf8f7d604a2965aec72b97d53920c93
DemuraAIdev <cyberaioff@gmail.com>
2023-01-18 11:47:42 +0700

    Mobile Stats support

--- components/Analys/index.js
+++ components/Analys/index.js
@@ -3,7 +3,7 @@ import GIthubFoll from "./GIthubFoll";
 
 export default function Index() {
   return (
-    <div className="grid grid-cols-2 gap-3">
+    <div className="grid grid-cols-2 gap-3 sm:grid-cols-1 ">
       <GIthub />
       <GIthubFoll />
     </div>
