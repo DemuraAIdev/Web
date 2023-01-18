dc681110907f783e69066c78227245f07fa3f9de
DemuraAIdev <cyberaioff@gmail.com>
2023-01-18 13:10:09 +0700

    Rename manifest.json to .webmanifest

--- pages/_document.js
+++ pages/_document.js
@@ -1,10 +1,14 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from "next/document";
 class MyDocument extends Document {
   render() {
     return (
       <Html lang="en" className="scroll-smooth selection:bg-primary-800">
         <Head>
-          <link rel="apple-touch-icon" sizes="76x76" href="/static/favicons/apple-touch-icon.png" />
+          <link
+            rel="apple-touch-icon"
+            sizes="76x76"
+            href="/static/favicons/apple-touch-icon.png"
+          />
           <link
             rel="icon"
             type="image/png"
@@ -18,12 +22,27 @@ class MyDocument extends Document {
             href="/static/favicons/favicon-16x16.png"
           />
           <link rel="shortcut icon" href="/static/favicons/favicon.ico" />
-          <link rel="manifest" href="/manifest.json" />
-          <link rel="mask-icon" href="/static/favicons/safari-pinned-tab.svg" color="#5bbad5" />
+          <link rel="manifest" href="/manifest.webmanifest" />
+          <link
+            rel="mask-icon"
+            href="/static/favicons/safari-pinned-tab.svg"
+            color="#5bbad5"
+          />
           <meta name="msapplication-TileColor" content="#000000" />
-          <meta name="msapplication-config" content="/static/favicons/browserconfig.xml" />
-          <meta name="theme-color" media="(prefers-color-scheme: light)" content="#fff" />
-          <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#000" />
+          <meta
+            name="msapplication-config"
+            content="/static/favicons/browserconfig.xml"
+          />
+          <meta
+            name="theme-color"
+            media="(prefers-color-scheme: light)"
+            content="#fff"
+          />
+          <meta
+            name="theme-color"
+            media="(prefers-color-scheme: dark)"
+            content="#000"
+          />
           <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
         </Head>
         <body className="bg-white text-black antialiased dark:bg-black dark:text-white">
@@ -31,8 +50,8 @@ class MyDocument extends Document {
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
 
-export default MyDocument
+export default MyDocument;
