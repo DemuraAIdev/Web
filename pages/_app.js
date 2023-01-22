db095ca821fd1267d1cf65fc5db0d8429a7c6412
DemuraAIdev <cyberaioff@gmail.com>
2023-01-22 16:28:06 +0700

    Ignore next auth

--- pages/_app.js
+++ pages/_app.js
@@ -1,30 +1,29 @@
-import '@/css/tailwind.css'
-import '@/css/prism.css'
-import '@/css/twemoji.css'
-import 'katex/dist/katex.css'
+import "@/css/tailwind.css";
+import "@/css/prism.css";
+import "@/css/twemoji.css";
+import "katex/dist/katex.css";
 
-import '@fontsource/inter/variable-full.css'
-import React from 'react'
-import { ThemeProvider } from 'next-themes'
-import Head from 'next/head'
-import siteMetadata from '@/data/siteMetadata'
-import Analytics from '@/components/analytics'
-import LayoutWrapper from '@/components/LayoutWrapper'
-import { ClientReload } from '@/components/ClientReload'
-import { SessionProvider } from 'next-auth/react'
+import "@fontsource/inter/variable-full.css";
+import { ThemeProvider } from "next-themes";
+import Head from "next/head";
+import siteMetadata from "@/data/siteMetadata";
+import Analytics from "@/components/analytics";
+import LayoutWrapper from "@/components/LayoutWrapper";
+import { ClientReload } from "@/components/ClientReload";
+import { SessionProvider } from "next-auth/react";
 
-import NProgress from 'nprogress' //nprogress module
-NProgress.configure({ showSpinner: false })
-import '@/css/Nprogress.css' //styles of nprogress
+import NProgress from "nprogress"; //nprogress module
+NProgress.configure({ showSpinner: false });
+import "@/css/Nprogress.css"; //styles of nprogress
 
-import Router from 'next/router'
+import Router from "next/router";
 
-Router.events.on('routeChangeStart', () => NProgress.start())
-Router.events.on('routeChangeComplete', () => NProgress.done())
-Router.events.on('routeChangeError', () => NProgress.done())
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
 
-const isDevelopment = process.env.NODE_ENV === 'development'
-const isSocket = process.env.SOCKET
+const isDevelopment = process.env.NODE_ENV === "development";
+const isSocket = process.env.SOCKET;
 
 export default function App({ Component, pageProps }) {
   return (
@@ -40,5 +39,5 @@ export default function App({ Component, pageProps }) {
         </LayoutWrapper>
       </ThemeProvider>
     </SessionProvider>
-  )
+  );
 }
